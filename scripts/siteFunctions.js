8f72bf615cebeeb1a47653e429478782e26d863a
Philipp <79584460+ThePJK@users.noreply.github.com>
2021-02-24 15:06:25 +0100

    Update siteFunctions.js

--- scripts/siteFunctions.js
+++ scripts/siteFunctions.js
@@ -1,12 +1,12 @@
 // **************************************************
-// Seite öffnen und schließen
+// Open/close page
 // **************************************************
 window.onload = function(){sendStatement("launched", document.title);};
 
 window.onbeforeunload = function(){sendStatement("terminated", document.title);};
 
 // **************************************************
-// Eventlistener für Buttons
+// Eventlistener for buttons
 // **************************************************
 
 var btn1 = document.getElementById("btn1");
@@ -16,7 +16,7 @@ var btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", function(){sendStatement("pressed", "Button2");});
 
 // **************************************************
-// Eventlistener für lokales Video
+// Eventlistener for local video
 // **************************************************
 
 var videoLocal = document.getElementById("videoLocal");
