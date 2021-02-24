60555b25b8577f0c6bcbb7d4c4e388f4eac72ed3
Philipp <79584460+ThePJK@users.noreply.github.com>
2021-02-24 15:02:32 +0100

    Update configuration.js

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -16,7 +16,7 @@ globalSettings.debuggingObjectID				= 'BeispielID';
 
 var lrsSettings = new Object();
 
-lrsSettings.endpointUrl      					= 'https://lrs-dev.knowhow.de/data/xAPI/statements';
+lrsSettings.endpointUrl      					= 'https://example.com/data/xAPI/statements';
 lrsSettings.version 							= '1.0.3';
-lrsSettings.user								= '9a87fefff4e7e4b9e483560a096827942ba616b0';
-lrsSettings.password							= 'f03ff346613a8d82851de7d2e417147ddd5c217c';
+lrsSettings.user								= 'USER';
+lrsSettings.password							= 'PASSWORD';
