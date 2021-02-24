38717e1ad0ea718a71fdc82e2e78fd1aef6fd5df
Philipp <79584460+ThePJK@users.noreply.github.com>
2021-02-24 15:05:00 +0100

    Update configuration.js
    translated german comments to english

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -1,22 +1,22 @@
 // **************************************************
-// Globale Einstellungen
+// Global Config
 // **************************************************
 
 var globalSettings = new Object();
 
-globalSettings.debuggingEnabled   				= true;
-globalSettings.debuggingUserVorname				= 'Max'
-globalSettings.debuggingUserNachname			= 'Mustermann'
-globalSettings.debuggingObjectID				= 'BeispielID';
+globalSettings.debuggingEnabled      = true;
+globalSettings.debuggingUserVorname  = 'Max'
+globalSettings.debuggingUserNachname = 'Mustermann'
+globalSettings.debuggingObjectID     = 'ExampleID';
 
 
 // **************************************************
-// xAPI / LRS Einstellungen
+// xAPI / LRS Config
 // **************************************************
 
 var lrsSettings = new Object();
 
-lrsSettings.endpointUrl      					= 'https://example.com/data/xAPI/statements';
-lrsSettings.version 							= '1.0.3';
-lrsSettings.user								= 'USER';
-lrsSettings.password							= 'PASSWORD';
+lrsSettings.endpointUrl = 'https://example.com/data/xAPI/statements';
+lrsSettings.version     = '1.0.3';
+lrsSettings.user        = 'USER';
+lrsSettings.password    = 'PASSWORD';
